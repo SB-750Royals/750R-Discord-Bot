54a7cd5cf7219b0c101a1496880168418b885323
vigneshsaravanakumar404 <10021913@sbstudents.ir>
2023-03-19 15:21:45 -0400

    IT FUCKING WORKS :)

--- bot-env/Scripts/main.py
+++ bot-env/Scripts/main.py
@@ -1,4 +1,22 @@
-import bot
+import discord
+from discord.ext import commands
+
+import config
 
 if __name__ == '__main__':
-    bot.run_discord_bot()
+    client = discord.ext.commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+    @client.event
+    async def on_ready():
+        await client.change_presence(
+            activity=discord.Game(f'Latency: {(client.latency * 1000):.3f} ms'))  # Latency in seconds
+        print('Bot is ready!')
+
+
+    @client.command()
+    async def ping(ctx):
+        await ctx.send(f'Pong! Latency: {(client.latency * 1000):.3f} ms')
+
+
+    client.run(config.TOKEN)
